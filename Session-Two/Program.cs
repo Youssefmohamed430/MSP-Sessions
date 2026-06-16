487962cecd3055b2e39b1167a1c41ce7627bedc5
Yusef <youssefmohmed430@gmail.com>
2026-06-16 19:56:08 +0300

    .

--- Session-Two/Program.cs
+++ Session-Two/Program.cs
@@ -1,2 +1,23 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿/*
+                         ========>>>>> TO DO: <<<<<<========
+
+    1. Explain Project Structure and Files
+    2. The Difference between App Project and Class Library Project.
+    3. The Isolation between the projects at the same solution.
+    4. Explain How Namespaces work in C# and how to use them to avoid naming conflicts.
+    5. Explain Why Using Namespaces is Important in Large Projects and How it Helps in Organizing Code.
+ 
+ */
+
+using System;
+
+namespace Session_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
