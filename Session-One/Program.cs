1b92c35d49efe390ed987f2f789a02e8f6ee0852
Yusef <youssefmohmed430@gmail.com>
2026-06-28 10:54:00 +0300

    edit

--- Session-One/Program.cs
+++ Session-One/Program.cs
@@ -22,35 +22,40 @@ namespace Session_One
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //Console.Write("MSP");
 
-            int x = 10;
-            string y = "Hello, World!"; // Store address (Pointer) at stack and the actual content at heap because it is an obj
+            //data type   variable name
+            //int ,  long, double ,float ,decimal 10.43243245454 ,bool = true , false
+            // char
+            //char n = 'f';
+            //int         x              = 10;
+            //string y = "Hello, World!"; // Store address (Pointer) at stack and the actual content at heap because it is an obj
 
-            string firstName = "Youssef";
-            string lastName = "Mohmed";
+            //string firstName = "Youssef";
+            //string lastName = "Mohmed";
 
-            string fullName_1 = $"{firstName} {lastName}"; // Interpolation
-            string fullName_2 = firstName + " " + lastName; // Concatenation
+            //string fullName_1 = $"The full name is {firstName} {lastName}"; // Interpolation
+            //string fullName_2 = firstName + " " + lastName; // Concatenation
 
-            Console.WriteLine(fullName_1);
-            Console.WriteLine(fullName_2);
+            //Console.WriteLine(fullName_1);
+            //Console.WriteLine(fullName_2);
 
-            // The Implicit Engine : The Compiler analyzes the right side of the expression and
-            // convert it to the right data type at compiletime
+            //// The Implicit Engine : The Compiler analyzes the right side of the expression and
+            //// convert it to the right data type at compiletime
 
             var z = 10;
             Console.WriteLine(z);
 
-            // Dynamic Keyword : Determination of the data type to the variable is at Runtime
+            //// Dynamic Keyword : Determination of the data type to the variable is at Runtime
 
             //int a = 5;
             //a = "text"; // Error
 
-            //dynamic a = 5;
-            //a = "text"; // OK
-            //a = true;   // OK
+            dynamic a = 5;
+            a = "text"; // OK
+            a = true;   // OK
 
-            // Operators
+            //// Operators
 
             int sum = 2 + 3;
             int sub = 3 - 2;
@@ -58,37 +63,39 @@ namespace Session_One
             int mul = 4 * 2;
             int power = 4 ^ 2;
 
-            // Logical Operators
+            //// Logical Operators
             bool flag = true && false;
             flag = true || false;
             flag = !true;
 
-            //XOR
+            ////XOR
             flag = false ^ true;
-            // true ^ true => false
-            // true ^ false => true
-            // false ^ true => true
-            // false ^ false => false
+            //// true ^ true => false
+            //// true ^ false => true
+            //// false ^ true => true
+            //// false ^ false => false
 
-            // Comparison Operators
-            flag = 1 == 0;
-            flag = 1 != 0;
-            flag = 1 > 0;
-            flag = 1 >= 0;
-            flag = 1 < 0;
-            flag = 1 <= 0;
+            //// Comparison Operators
+            //flag = 1 == 0;
+            //flag = 1 != 0;
+            //flag = 1 > 0;
+            //flag = 1 >= 0;
+            //flag = 1 < 0;
+            //flag = 1 <= 0;
 
-            double gpa = 3.0;
+            double gpa = 4.1;
             string university = "Helwan";
             bool IsAccepted = (gpa < 4.0 && gpa > 3.4) || university == "Cairo";
             Console.WriteLine(IsAccepted);
 
-            // Explain Short Circuit Concept
+            //(user != null || fun(user))
+
+            //// Explain Short Circuit Concept
             flag = true || false;// => true direct without complete exp
             flag = false && true;// => false direct without complete exp
             // Standard Bitwise Logical (| - &) =>> force to complete the exp
 
-            
+
 
 
         }
