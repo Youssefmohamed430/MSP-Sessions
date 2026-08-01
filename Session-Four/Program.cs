a810cee1843ad3b6207fba4893bcf30dbd394f35
Yusef <youssefmohmed430@gmail.com>
2026-08-01 08:47:52 +0300

    .

--- Session-Four/Program.cs
+++ Session-Four/Program.cs
@@ -4,23 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] array = new int[10];
-        Console.WriteLine(array.Length);
+        //int[] array = new int[10];
+        //Console.WriteLine(array.Length);
 
-        List<int> list = new List<int>(5); // Dynamic array
-        Console.WriteLine(list.Capacity);
+        //List<int> list = new List<int>(5); // Dynamic array
+        //Console.WriteLine(list.Capacity);
 
-        list.Add(1);
-        list.Add(2);
-        list.Add(3);
-        list.Add(4);
-        list.Add(5);
-        list.Add(6);
-        Console.WriteLine(list.Count);
-        for (int i = 0; i < list.Count; i++)
-        {
-            Console.WriteLine(list[i]);
-        }
+        //list.Add(1);
+        //list.Add(2);
+        //list.Add(3);
+        //list.Add(4);
+        //list.Add(5);
+        //list.Add(6);
+        //Console.WriteLine(list.Count);
+        //for (int i = 0; i < list.Count; i++)
+        //{
+        //    Console.WriteLine(list[i]);
+        //}
 
         //List<int> list1 = new List<int>(10);
         //list1.Add(8);
@@ -47,64 +47,119 @@ class Program
         //var list2 = list1.ToArray();
         //Console.WriteLine(list2);
 
-        LinkedList<int> l = new LinkedList<int>();
+        //LinkedList<int> l = new LinkedList<int>();
 
-        l.AddFirst(1);
-        l.AddFirst(2);
-        l.AddFirst(3);
-        l.AddFirst(4);
-        l.AddFirst(5);
-        Console.WriteLine(l.First + " " + l.Last);
-        l.AddLast(1);
-        Console.WriteLine(l.Last);
-        Console.WriteLine(l.Count);
-        l.RemoveFirst();
-        l.RemoveLast();
-        l.Clear();
-
-
-        Stack<int> stack = new Stack<int>();
-        stack.Push(1);
-        stack.Push(2);
-        stack.Push(3);
-        stack.Push(4);
-        stack.Push(5);
-        Console.WriteLine(stack.Peek());
-        Console.WriteLine(stack.Pop());
-        Console.WriteLine(stack.Count);
-        var element = stack.Pop();
-        Console.WriteLine(element);
+        //l.AddFirst(1);
+        //l.AddFirst(2);
+        //l.AddFirst(3);
+        //l.AddFirst(4);
+        //l.AddFirst(5);
+        //Console.WriteLine(l.First + " " + l.Last);
+        //l.AddLast(1);
+        //Console.WriteLine(l.Last);
+        //Console.WriteLine(l.Count);
+        //l.RemoveFirst();
+        //l.RemoveLast();
+        //l.Clear();
 
 
-        Queue<int> queue = new Queue<int>();
-        queue.Enqueue(1);
-        queue.Enqueue(2);
-        queue.Enqueue(3);
-        queue.Enqueue(4);
-        var first = queue.Dequeue();
-        Console.WriteLine(first);
-        Console.WriteLine(queue.Peek());
+        //Stack<int> stack = new Stack<int>();
+        //stack.Push(1);
+        //stack.Push(2);
+        //stack.Push(3);
+        //stack.Push(4);
+        //stack.Push(5);
+        //Console.WriteLine(stack.Peek());
+        //Console.WriteLine(stack.Pop());
+        //Console.WriteLine(stack.Count);
+        //var element = stack.Pop();
+        //Console.WriteLine(element);
 
-        HashSet<int> set = new HashSet<int>();
-        set.Add(1);
-        set.Add(2);
-        set.Add(3);
-        set.Add(1);
 
-        foreach (var item in set)
-        {
-            Console.WriteLine(item);
-        }
+        //Queue<int> queue = new Queue<int>();
+        //queue.Enqueue(1);
+        //queue.Enqueue(2);
+        //queue.Enqueue(3);
+        //queue.Enqueue(4);
+        //var first = queue.Dequeue();
+        //Console.WriteLine(first);
+        //Console.WriteLine(queue.Peek());
+
+        //HashSet<int> set = new HashSet<int>();
+        //set.Add(1);
+        //set.Add(2);
+        //set.Add(3);
+        //set.Add(1);
+
+        //foreach (var item in set)
+        //{
+        //    Console.WriteLine(item);
+        //}
         
 
-        Dictionary<int,string> dict = new Dictionary<int,string>();
-        dict.Add(1, "Apple");
-        dict.Add(2, "Orange");
-        dict.Add(3, "Mango");
+        //Dictionary<int,string> dict = new Dictionary<int,string>();
+        //dict.Add(1, "Apple");
+        //dict.Add(2, "Orange");
+        //dict.Add(3, "Mango");
 
-        foreach (var item in dict)
+        //foreach (var item in dict)
+        //{
+        //    Console.WriteLine(item.Key + " " + item.Value);
+        //}
+        // -----------------------------------------------------------------------------------------------
+        List<User> users = new List<User>();
+
+        var user1 = new User();
+        user1.Id = 1;
+        user1.Name = "User1";
+        user1.Username = "UserOne";
+        user1.Password = "User123";
+        user1.Email = "User1@gmail.com";
+        user1.Role = "User";
+
+        var user2 = new User();
+        user2.Id = 2;
+        user2.Name = "User2";
+        user2.Username = "UserTwo";
+        user2.Password = "User234";
+        user2.Email = "User2@gmail.com";
+        user2.Role = "Admin";
+
+        var user3 = new User();
+        user3.Id = 3;
+        user3.Name = "User3";
+        user3.Username = "UserThree";
+        user3.Password = "User345";
+        user3.Email = "User3@gmail.com";
+        user3.Role = "User";
+
+        var user4 = new User();
+        user4.Id = 4;
+        user4.Name = "User4";
+        user4.Username = "UserFour";
+        user4.Password = "User456";
+        user4.Email = "User4@gmail.com";
+        user4.Role = "Admin";
+
+        users.Add(user1);
+        users.Add(user2);
+        users.Add(user3);
+        users.Add(user4);
+
+        foreach(var user in users)
         {
-            Console.WriteLine(item.Key + " " + item.Value);
+            Console.WriteLine(user);
         }
+
+        // --------------------------------------- LINQ ------------------------------------------------
+        
+        var ListOfUsers = users.Where(x => x.Role == "User").ToList();
+        ListOfUsers.ForEach(x => Console.WriteLine(x));
+        
+        var FirstUser = users.First(x => x.Role == "User2");
+        Console.WriteLine(FirstUser);
+
+        var FirstUserId = users.First(x => x.Id == 1);
+        Console.WriteLine();
     }
 }
---
+++ Session-Four/User.cs
@@ -0,0 +1,15 @@
+﻿namespace Session_Four;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    public override string ToString()
+    {
+        return $"Name : {this.Name} | UserName : {this.Username} | Password : {this.Password} | Email : {this.Email} | Role : {this.Role}";
+    }
+}
